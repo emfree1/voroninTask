552a95b188ec24011b2c40432130aa9fb2108bc0
emfree1 <emfree20@gmail.com>
2020-10-04 19:28:49 +0300

    finish

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-  
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 700);
+}
+
 const swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
